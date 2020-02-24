891f2060c721ca902088c30a61d5a4ea0acdc4aa
xbblwf8 <ankur.rohit@bnymellon.com>
2020-02-24 16:24:16 +0530

    query and criteria class

--- src/main/java/com/company/project/controller/EmployeeController.java
+++ src/main/java/com/company/project/controller/EmployeeController.java
@@ -4,6 +4,9 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,26 +22,37 @@ public class EmployeeController {
 
 	@Autowired
 	EmployeeRepository er;
-	
+
+	@Autowired
+	MongoTemplate mongoTemplate;
+
 	@PostMapping("/saveEmployee")
 	public String saveEmployee(@RequestBody Employee e) {
 		er.save(e);
 		return "Employee saved with id = " + e.get_id();
 	}
-	
+
 	@GetMapping("/getAllEmployees")
 	public List<Employee> getAllEmployees() {
 		return er.findAll();
 	}
-	
+
 	@GetMapping("/getEmployee/{id}")
 	public Optional<Employee> getEmployeeById(@PathVariable int id) {
 		return er.findEmployeeById(id);
 	}
-	
+
 	@DeleteMapping("/deleteEmployee/{id}")
 	public String deleteEmployee(@PathVariable int id) {
 		er.deleteById(id);
 		return "Employee with id " + id + " deleted";
+	}
+
+	@GetMapping("/getEmployeesUnderManager/{managerName}")
+	public List<Employee> getEmployeeAllDetails(@PathVariable String managerName) {
+		Query query = new Query();
+		query.addCriteria(Criteria.where("managerName").is(managerName));
+		List<Employee> employees = mongoTemplate.find(query, Employee.class);
+		return employees;
 	}
 }
--- src/main/java/com/company/project/model/Address.java
+++ src/main/java/com/company/project/model/Address.java
@@ -1,10 +1,16 @@
 package com.company.project.model;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
 import lombok.Data;
 
 @Data
+//@Document(collection = "Address")
 public class Address {
 
+//	@Id
+//	String id;
 	String flatNo;
 	String society;
 	String city;
