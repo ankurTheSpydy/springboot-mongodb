a1c19387195d64c3d488c3c5aec0c10fc923d5f0
xbblwf8 <ankur.rohit@bnymellon.com>
2020-02-24 16:29:18 +0530

    refactoring

--- src/main/java/com/company/project/model/Address.java
+++ src/main/java/com/company/project/model/Address.java
@@ -1,16 +1,10 @@
 package com.company.project.model;
 
-import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
-
 import lombok.Data;
 
 @Data
-//@Document(collection = "Address")
 public class Address {
 
-//	@Id
-//	String id;
 	String flatNo;
 	String society;
 	String city;
